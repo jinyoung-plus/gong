e41b5a3837c29a090fbd7033704faca017c3f00e
jinyoung-plus <tipss00@naver.com>
2023-12-16 15:00:36 +0100

    add png2

--- src/app/our-venues/our-venues.component.ts
+++ src/app/our-venues/our-venues.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-our-venues',
   templateUrl: './our-venues.component.html',
   styleUrl: './our-venues.component.css'
 })
-export class OurVenuesComponent {
+export class OurVenuesComponent implements OnInit {
+  venues = [
+    {
+      name: 'Grand Ballroom',
+      imageUrl: 'assets/grand-ballroom.jpg',
+      description: 'An elegant space for large events and conferences.'
+    },
+    // ... 다른 venues 데이터
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
